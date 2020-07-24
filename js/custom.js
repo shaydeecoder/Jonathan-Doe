2593d7b8f65fd9c4dbfff64bf26170ec40c089d9
shaydeecoder <shaydeecoder@gmail.com>
2020-07-24 16:15:40 +0100

    added popup to portfolio section

--- js/custom.js
+++ js/custom.js
@@ -8,3 +8,19 @@ $(function(){
     time: 2000
   });
 });
+
+// PORTFOLIO
+$(document).ready(function() {
+  $('.image-link').magnificPopup({type:'image'});
+});
+
+$(function() {
+  $("#portfolio").magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    // other options
+    gallery:{
+      enabled:true
+    }
+  });
+});
